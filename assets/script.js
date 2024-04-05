82ad4fddfce3de409aab2b71073394a9df07d124
alfandialamshach <fandixpc1@gmail.com>
2024-04-05 15:32:58 +0700

    sudah selesai

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("Ini Video Rindu Rumah");
+})
+
+audio.addEventListener("click", function audio() {
+    alert("Ini Audio Rindu Rumah");
+})
+
+gambar.addEventListener("click", function gambar() {
+    alert("Ini Gambar Rindu Rumah");
+})
